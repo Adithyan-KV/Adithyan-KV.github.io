e3e92124b1e891c9e888e778a0b62e323d4a6567
Adithyan-KV <adithyankv007@gmail.com>
2020-08-26 03:00:07 +0530

    Added features to demo

--- player-component.js
+++ player-component.js
@@ -12,6 +12,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
         connectedCallback() {
             console.log("Added to DOM");
+
+            let remove_button = this.shadowRoot.querySelector("#deny");
+            remove_button.addEventListener('click', () => {
+                let parent_box = this.shadowRoot.querySelector('.player-box');
+                parent_box.classList.add('removed')
+                parent_box.addEventListener('animationend', () => {
+                    this.remove();
+                })
+            })
+
+            let add_button = this.shadowRoot.querySelector("#accept");
+            add_button.addEventListener('click', () => {
+                let parent_box = this.shadowRoot.querySelector('.player-box');
+                let status = this.shadowRoot.querySelector('.status');
+                status.innerHTML = 'joined';
+                parent_box.classList.add('joined');
+            })
         }
 
         disconnectedCallback() {
